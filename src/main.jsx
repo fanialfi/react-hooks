e98b2c245576adde889cc499dfcb677a36f790e8
fanialfi <fanialfirdaus@gmail.com>
2024-11-08 00:12:50 +0700

    add new dependencies router dom for react web app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,28 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import LoginPage from "./pages/loginPage";
+import { Navigate } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/dashboard",
+    children: [
+      {
+        path: "products",
+        element: <h1>this is products page</h1>,
+      },
+      {
+        index: true,
+        element: <Navigate to={"/dashboard/products"} replace />,
+      },
+    ],
+  },
+]);
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <h1>Hello World</h1>
-  </StrictMode>
+  <RouterProvider router={router} />
 );
---
+++ src/pages/loginPage.jsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+  return <h1 className="text-center">Login Page</h1>;
+}
